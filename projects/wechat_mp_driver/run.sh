bdf2e930b0d8b0ae3f6a2e4f63d5e061919cb702
Jackon Yang <i@jackon.me>
2021-11-23 02:25:11 +0800

    project link fix

--- projects/wechat_mp_driver/run.sh
+++ projects/wechat_mp_driver/run.sh
@@ -1,4 +1,4 @@
-env_file='../../.private-data/wechat-mp-driver.env'
+env_file='../../.private-data/wechat_mp_driver.env'
 
 if [ -f "$env_file" ]; then
     env $(cat $env_file | xargs) python3 demos.py
