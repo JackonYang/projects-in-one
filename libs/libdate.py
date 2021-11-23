250458dca7603208e4c504a573e33f04fc5bdec9
Jackon Yang <i@jackon.me>
2021-11-24 01:00:38 +0800

    add libdate

---
+++ libs/libdate.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+
+
+def today(hours=8, formatter='%Y-%m-%d'):
+    t = datetime.now(timezone(timedelta(hours=hours)))
+    return t.strftime(formatter)
+
+
+def yesterday(hours=8, formatter='%Y-%m-%d'):
+    t_today = datetime.now(timezone(timedelta(hours=hours)))
+    t = t_today - timedelta(days=1)
+    return t.strftime(formatter)
+
+
+def readable_now(hours=8, formatter='%Y-%m-%d %H:%M:%S'):
+    t = datetime.now(timezone(timedelta(hours=hours)))
+    return t.strftime(formatter)
+
+
+if __name__ == '__main__':
+    print('today: %s' % today())
+    print('yesterday: %s' % yesterday())
+    print('readable_now: %s' % readable_now())
