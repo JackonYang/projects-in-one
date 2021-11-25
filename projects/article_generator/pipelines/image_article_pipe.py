18c74c18cb02f0e2d95ff3deae1dc00aec546eba
Jackon Yang <i@jackon.me>
2021-11-26 02:24:25 +0800

    templates and cache upgrade

--- projects/article_generator/pipelines/image_article_pipe.py
+++ projects/article_generator/pipelines/image_article_pipe.py
@@ -1,6 +1,7 @@
 import os
 
 from crawlers.wechat_mp_images.api import download_images
+from libs.libmd5 import md5_for_text
 from wechat_mp_driver.api import upload_local_image_in_article
 from libs.libcache.api import jcache
 from libs.libdate import today
@@ -18,8 +19,8 @@ def upload_image_to_mp(image, appid, secret):
 
 
 class ImageArticlePipe(PipelineBase):
-    def get_data(self, src_url, article_unique_key, upload_params, **kwargs):
-        image_dir = os.path.join(donwloaded_images_dir, article_unique_key)
+    def get_data(self, src_url, upload_params, **kwargs):
+        image_dir = os.path.join(donwloaded_images_dir, md5_for_text(src_url))
         image_paths = download_images(src_url, image_dir)
         image_urls = self.upload_images(image_paths[1:-1], upload_params)
         return {
