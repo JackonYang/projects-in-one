bc51b55bac9c3a0ecff03bc764f5a498727dac67
Jackon Yang <i@jackon.me>
2021-11-25 09:14:42 +0800

    add libs

---
+++ libs/libopenfile.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8-*-
+import time
+import subprocess
+import os
+import sys
+
+def open_file(filename):
+    platform_cmd = {
+            'win32': 'start',  # win7 32bit, win7 64bit
+            'cygwin': 'start',  # cygwin
+            'linux2': 'xdg-open',  # ubuntu 12.04 64bit
+            'darwin': 'open',  # Mac
+            }
+    if sys.platform.startswith('win'):  # windows
+        os.startfile(filename)
+    else:
+        subprocess.call((platform_cmd.get(sys.platform, 'xdg-open'), filename))
---
+++ libs/libtext_cleaning/html_text.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import bleach
+from html.parser import HTMLParser
+
+from .spaces import (
+    remove_continuous_spaces,
+)
+
+from .string_literals import (
+    remove_extra_literal_escapes,
+)
+
+
+h = HTMLParser()
+
+tags_cleaner = bleach.Cleaner(
+    tags=[],
+    attributes=[],
+    strip=True,
+    filters=[]
+)
+
+
+def convert_html_to_text(text, merge_continuous_spaces=True):
+    if not text:
+        return text
+
+    # clean html entity first
+    text = h.unescape(text)
+
+    text = remove_extra_literal_escapes(text)
+
+    # clean ALL html tags
+    text = tags_cleaner.clean(text)
+
+    if merge_continuous_spaces:
+        text = remove_continuous_spaces(text)
+
+    return text
