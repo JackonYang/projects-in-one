5d80031ebf8be40261aaf00ea0039c1d4473ee9d
JackonYang <i@jackon.me>
2022-01-25 00:26:59 +0800

    collect static

--- projects/writer-infra-2021/start_prod.sh
+++ projects/writer-infra-2021/start_prod.sh
@@ -1,5 +1,8 @@
 env_file='../../.private-data/writer-infra-2021.env'
 
+# collect static
+python3 manage.py collectstatic
+
 if [ -f "$env_file" ]; then
     env $(cat $env_file | xargs) uwsgi --ini deploy/uwsgi/uwsgi.ini
 else
