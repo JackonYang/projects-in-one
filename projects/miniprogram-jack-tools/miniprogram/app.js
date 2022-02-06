31c222e22ebfd5810507dc9a5dd2f07699e20c04
Jackon Yang <i@jackon.me>
2022-02-06 13:28:31 +0800

    common style and config fix

--- projects/miniprogram-jack-tools/miniprogram/app.js
+++ projects/miniprogram-jack-tools/miniprogram/app.js
@@ -1,5 +1,46 @@
 // app.js
+import request from './models/request.js';
+import utils from './utils/util.js';
+import dates from '/utils/dates';
+import sources from '/utils/source';
+import packages from '/utils/package';
+import base64 from '/utils/base64';
+import english from '/i18n/en.js';
+import chinese from '/i18n/zh.js';
+
+// var sensors = require('/sensors/sensorsdata.js');
+// sensors.init();
+
 App({
+  request: request,
+  utils: utils,
+  dates: dates,
+  sources: sources,
+  packages: packages,
+  Base64: base64,
+  english: english,
+  chinese: chinese,
+
+  // 全局参数
+  globalData: {
+    token: '',
+    handLoginState: false,
+    loginData: null,
+    langInfo: null,
+    systemInfo: null,
+    userInfo: null,
+    encryptInfo: null,
+    authInfo: null,
+    curScene: ''
+  },
+  // 页面间数据传递
+  pageBetweenData: {
+    currentMergeInfo: '',
+    FindFaceInfo: '',
+    mergeMakeInfo: '',
+    currentAlbumInfo: ''
+  },
+
   onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
