8884aea092468dfce71f619ff1c635bcc0be4f6c
Jackon Yang <i@jackon.me>
2022-03-25 01:49:51 +0800

    sanguo page v1

---
+++ projects/miniapp-jack-tools-simple/miniprogram/pages/sanguo/components/hero-brief-box/hero-brief-box.js
@@ -0,0 +1,29 @@
+// pages/sanguo/components/hero-brief-box/hero-brief-box.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    heroInfo: Object,
+    // heroName: String,
+    // heroPosition: String,
+    // heroSkill0: String,
+    // heroSkill1: String,
+    // heroSkill2: String,
+    // heroPointAdd: String,
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
--- projects/miniapp-jack-tools-simple/miniprogram/pages/sanguo/heroTeams/heroTeams.js
+++ projects/miniapp-jack-tools-simple/miniprogram/pages/sanguo/heroTeams/heroTeams.js
@@ -5,14 +5,24 @@ Page({
    * Page initial data
    */
   data: {
-
+    teamsData: [],
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    var that = this;
+    const db = wx.cloud.database();
+    db.collection('game-sanguo-teams').get({
+      success: function(res) {
+        // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
+        // console.log(res.data)
+        that.setData({
+          teamsData: res.data,
+        });
+      }
+    })
   },
 
   /**
