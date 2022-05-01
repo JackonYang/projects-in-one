b6dadf5ec93d5d2297976db99fb1547cd24b3ec0
Jackon Yang <i@jackon.me>
2022-05-01 19:21:12 +0800

    add flame_graph command

--- projects/j-commands/j-commands.sh
+++ projects/j-commands/j-commands.sh
@@ -1,8 +1,9 @@
-function j() {
+script_path=`readlink -f ~/.j-commands.sh`
+project_dir=`dirname $script_path`
+
+function j () {
 	func_name=$1
 	echo $func_name
 }
 
-function new() {
-
-}
+alias flame_graph="bash $project_dir/plugins/flame_graph.sh"
